41a2580c0d118bc5abb328b80d0070cd292c2889
Jack Tang <jack.tangth@gmail.com>
2023-04-16 22:41:23 +0800

    adding image interception effect

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,19 +6,22 @@ import About from "./about";
 import Layout from "../components/home/layout";
 import { useInView } from "react-intersection-observer";
 import { useState } from "react";
-import { log } from "console";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const { ref, inView } = useInView({
+  const { ref:img1Ref, inView:img1InView } = useInView({
     threshold: 0.5,
   });
-  const [scrollTop, setScrollTop] = useState(0);
-  const handleScroll = (event:React.UIEvent<HTMLImageElement, UIEvent>) => {
-    console.log(event.currentTarget.scrollTop);
-    
-    setScrollTop(event.currentTarget.scrollTop);
-  };
+  const { ref:img2Ref, inView:img2InView } = useInView({
+    threshold: 0.5,
+  });
+  const { ref:img3Ref, inView:img3InView } = useInView({
+    threshold: 0.5,
+  });
+  const { ref:img4Ref, inView:img4InView } = useInView({
+    threshold: 0.5,
+  });
+
   return (
     <>
       <Layout />
@@ -109,39 +112,43 @@ export default function Home() {
       </div>
       <h3>About me:</h3>
       <div className="relative h-[3000px]">
-        <div className="absolute p-0.5 bg-slate-400  h-full right-1/2">
+        <div className="absolute p-0.5 bg-slate-400  h-full right-1/2 z-[3]">
           <div className="rounded-full bg-slate-100 h-5 w-5 absolute translate-x-[-50%]  top-0"></div>
         </div>
         <div className="bg-black story-content-left">
-          sdadsa{scrollTop}
+          sdadsa
         </div>
         <img
-        ref={ref}
-          className={`story-background-img  ${inView ?"transition-opacity":"opacity-20"}`}
+        ref={img1Ref}
+          className={`story-background-img  ${img1InView ?"transition-opacity":"opacity-20"}`}
           src="/spring.jpg"
           alt=""
         />
 
-        <div className="bg-black story-content-right top-1/4 ">
-          sdadsa
+        <div className="before:test   bg-transparent overflow-hidden story-content-right   top-[30%] ">
+        <div className="before:test1"></div>
+          <div className="test2">dsad</div>
+          
         </div>
         <img
-          className="story-background-img top-1/4 opacity-20"
+        ref={img2Ref}
+          className={`story-background-img top-1/4  ${img2InView ?"transition-opacity":"opacity-20"}`}
           src="/desert.jpg"
           alt=""
         />
 
         <div className="bg-black story-content-left top-2/4"></div>
         <img
-      
-          className="story-background-img top-2/4 opacity-20"
+      ref={img3Ref}
+      className={`story-background-img top-2/4  ${img3InView ?"transition-opacity":"opacity-20"}`}
           src="/tree.jpg"
           alt=""
         />
 
         <div className="bg-black story-content-right top-3/4"></div>
         <img
-          className="story-background-img top-3/4 opacity-20"
+          ref={img4Ref}
+          className={`story-background-img top-3/4  ${img4InView ?"transition-opacity":"opacity-20"}`}
           src="/winter.jpg"
           alt=""
         />
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,28 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: { screens: {
-    'xm': {'max': '639px'},
-    'sm': {'min': '640px', },
- 
-  },
-    extend: {backgroundImage: {
-      'magic': "linear-gradient(132deg, #5ddcff, #3c67e3 43%, #4e00c2)",
-   },},
+  theme: {
+    screens: {
+      'xm': { 'max': '639px' },
+      'sm': { 'min': '640px', },
+
+    },
+    extend: {
+      keyframes: {
+        'magic': {
+
+          'to': { 'transform': 'rotate(360deg)' },
+        },
+        'magic2': {
+
+          'to': { 'transform': 'rotate(-360deg)' },
+        }
+      },
+    },
   },
   plugins: [],
 }
