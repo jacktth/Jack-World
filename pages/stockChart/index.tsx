d981248fab0e631902e0ece9556ba348c7f2b3c8
Jack Tang <jack.tangth@gmail.com>
2023-05-04 01:08:13 +0800

    improve stockChart page css

--- pages/stockChart/index.tsx
+++ pages/stockChart/index.tsx
@@ -28,20 +28,20 @@ function StockChart() {
           <p>Stock Chart</p>
           <p>A single page web application for user to collect stock data</p>
           <div className="flex my-4 justify-center">
-          <Link
-                target="_blank"
-                className="project-source  "
-                href="https://github.com/jacktth/stock_chart"
-              >
-                Source Code
-              </Link>
-              <Link
-                target="_blank"
-                className="project-demo"
-                href="https://stock-chart.onrender.com"
-              >
-                Demo
-              </Link>
+            <Link
+              target="_blank"
+              className="project-source  "
+              href="https://github.com/jacktth/stock_chart"
+            >
+              Source Code
+            </Link>
+            <Link
+              target="_blank"
+              className="project-demo"
+              href="https://stock-chart.onrender.com"
+            >
+              Demo
+            </Link>
           </div>
         </div>
         <Image
@@ -51,8 +51,8 @@ function StockChart() {
         />
       </div>
 
-      <div className=" bg-blue-500 justify-around">
-        <p className="text-center text-5xl pt-5 xm:mb-5">Features</p>
+      <div className=" bg-[#7bdff2] justify-around">
+        <p className="text-center text-4xl text-black pt-5 xm:mb-5">Features</p>
         <div
           className="grid  gap-10  justify-around px-5 
         sm:grid-cols-3
@@ -68,8 +68,8 @@ function StockChart() {
 
             <p className="text-xl mb-4">Collect historical data</p>
             <p className="leading-8 tracking-wide text-lg">
-              The chart provide function for users to drag the data so as to
-              select and save the data in a specific period of time.
+              The chart provides a function that allows users to select data
+              from a specific time period, which can then be saved.
             </p>
           </div>
           <div className="stockChartFeatures">
@@ -80,8 +80,8 @@ function StockChart() {
             </div>
             <p className="text-xl mb-4">Review record quickly</p>
             <p className="leading-8 tracking-wide text-lg">
-              Users could click the their record to immediately display the
-              historical data of specific period on the chart.
+              Users can click on their saved records to immediately display
+              historical data from a specific period on the chart.
             </p>
           </div>
           <div className="stockChartFeatures">
@@ -92,90 +92,125 @@ function StockChart() {
             </div>
             <p className="text-xl mb-4">Open API</p>
             <p className="leading-8 tracking-wide text-lg">
-              The application aims for users to export their record and stock
-              historical data.
+              The application is designed to enable users to export historical
+              stock data from a specific period based on their saved records.
             </p>
           </div>
         </div>
       </div>
 
       {/*                   Motivation                          */}
-      <div className="bg-slate-200 text-black mb-4">
-        <p className="text-center text-5xl pt-8">My Motivations</p>
+      <div className="bg-[#f7a072] text-black ">
+        <p className="text-center text-4xl py-4">My Motivations</p>
         <div className="motivationContainer">
           <p className="motivationNumber">1.</p>
           <p className="motivationContent">
-            Most of the stock application only allow user to save the stock
-            symbol and view the stock data in the most recent time. However, I
-            want to also save the stock symbol with specific period of time.
-            Therefore, the application allows users to select and save the
-            specific period of time of the historical stock price.
+            Stock market applications typically allow users to save a stock
+            symbol and view its data starting from the most recent time period.
+            However, I believe it would be beneficial to enable users to save a
+            stock symbol along with a specific period of time. To accomplish
+            this, our application will provide the functionality for users to
+            select and save historical stock prices for a specific time period.
           </p>
         </div>
         <div className="motivationContainer">
           <p className="motivationNumber">2.</p>
           <p className="motivationContent">
-            If I want to review the stock pattern in the past 10 years in some
-            stock chart applications, I need to keep scrolling back from the
-            most recent date to the 10 years ago. It is not convenient. I desire
-            to have a function to record the pattern. Therefore, I can click the
-            record to display the data in the chart immediately.
+            When using some stock chart applications, reviewing the past stock
+            data requires scrolling back from the most recent date, which is
+            inconvenient. To address this, I would like a function that allows
+            me to save the pattern and display it in the chart with a single
+            click.
           </p>
         </div>
 
         <div className="motivationContainer">
           <p className="motivationNumber">3.</p>
           <p className="motivationContent">
-            I want to create my own dataset for stock pattern analysis.
-            Therefore, I need an application to help me to display the stock
-            price, record the stock patterns and and export the stock data.
+            I want to create my own dataset for stock pattern analysis, so I
+            need an application that can display stock prices, record stock
+            patterns, and export stock data.
           </p>
         </div>
       </div>
+      <div className="text-black bg-[#eff7f6] px-5 ">
+        {/* parent */}
+        <div className="grid   ">
+          {/* --------------------child-------------------- */}
+          <p className="text-center text-4xl py-3 ">Tech-Stack</p>
 
-      <div className="">
-        <h1>Tech-Stack</h1>
-        <div>
-          <div className="flex">
-            <SiReact className="fill-blue-500 stockChartIcon" />
-            <span>React</span>
+          <div className="bg-white grid grid-cols-4  w-full h-full relative">
+            {/* <div className="text-sm absolute top-[50%] left-[50%] h-full bg-black text-transparent
+            transform -translate-x-1/2 -translate-y-1/2 ">
+              x
+            </div> */}
+
+            <div className="projectToolTopic bg-gradient-to-r from-blue-200 to-purple-50">Frontend</div>
+            <div className="projectToolTopic bg-gradient-to-l from-green-200 to-red-50">Backend</div>
+
+            <div className="p-2 bg-gradient-to-tl text-xl from-blue-400 to-white ">
+              <div className="text-lg">
+                <div className="flex">
+                  <SiReact className="fill-blue-500 stockChartIcon " />
+                  <span>React</span>
+                </div>
+                <p className=" text-lg">The user interfaces library</p>
+              </div>
+
+              <div className="">
+                <div className="flex">
+                  <SiTailwindcss className="fill-blue-400 stockChartIcon" />
+                  <span>Tailwindcss</span>
+                </div>
+                <p  className=" text-lg">
+                  The CSS framework
+                </p>
+              </div>
+            </div>
+
+            <div className="p-2 bg-gradient-to-br text-xl from-orange-50 to-purple-400 ">
+              <div className="">
+                <div className="flex">
+                  <SiReactquery className="fill-amber-600 stockChartIcon" />
+                  <span>
+                    TanStack Query<span>{`(React Query)`}</span>
+                  </span>
+                </div>
+                <p className=" text-lg">
+                  The asynchronous state management tools
+                </p>
+              </div>
+
+              <div className="">
+                <div className="flex">
+                  <SiRedux className="fill-purple-600 stockChartIcon" />
+                  <span>Redux ToolKit</span>
+                </div>
+                <p className=" text-lg">The state management library</p>
+              </div>
+            </div>
+            <div className="p-2 border-l-2 border-l-black bg-gradient-to-br from-white to-red-600 text-xl">
+              <div className="flex ">
+                <SiNestjs className="fill-red-600 stockChartIcon" />
+                <span>Nestjs</span>
+              </div>
+              <p className=" text-lg">The backend framework</p>
+              <p className=" text-lg">
+                Handling server-side logic, including a RESTful API and
+                processing JSON data from third-party sources.
+              </p>
+            </div>
+            <div className="p-2 bg-gradient-to-br text-xl from-white to-green-400 ">
+              <div className="flex">
+                <SiSupabase className="fill-[#2F98CA] stockChartIcon" />
+                <span>Supabase</span>
+              </div>
+              <p className=" text-lg">The Backend-as-a-Service platform</p>
+              <p className=" text-lg">
+                Handling user authentication and serving as a database
+              </p>
+            </div>
           </div>
-          <p>The user interfaces library</p>
-        </div>
-        <div>
-          <div className="flex">
-            <SiTailwindcss className="fill-blue-400 stockChartIcon" />
-            <span>Tailwindcss</span>
-          </div>
-          <p>The CSS framework</p>
-        </div>
-        <div>
-          <div className="flex">
-            <SiReactquery className="fill-red-400 stockChartIcon" />
-            <span>React query<span>{`(FKA React Query)`}</span></span>
-          </div>
-          <p>The asynchronous state management tools</p>
-        </div>
-        <div>
-          <div className="flex">
-            <SiRedux className="fill-purple-600 stockChartIcon" />
-            <span>Redux ToolKit</span>
-          </div>
-          <p>The state management library</p>
-        </div>
-        <div>
-          <div className="flex">
-          <SiNestjs className="fill-red-600 stockChartIcon" />
-            <span>Nestjs</span>
-          </div>
-          <p>The backend framework for handling service side logic </p>
-        </div>
-        <div>
-          <div className="flex">
-          <SiSupabase className="fill-[#2F98CA] stockChartIcon" />
-            <span>Supabase</span>
-          </div>
-          <p>The Backend-as-a-Service platform</p>
         </div>
       </div>
     </>
